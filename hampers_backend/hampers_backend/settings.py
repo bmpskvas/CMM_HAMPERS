5bbc5a4162e76d21790729ce467ee5126d008bc6
bmpskvas <khushi41bansal@gmail.com>
2024-01-27 20:37:11 +0530

    css added to backend

--- hampers_backend/hampers_backend/settings.py
+++ hampers_backend/hampers_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -34,6 +34,8 @@ CORS_ORIGIN_WHITELIST = (
 )
 
 CSRF_TRUSTED_ORIGINS = ["https://cmmhampers-production.up.railway.app"]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
+STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 
 # Application definition
 
